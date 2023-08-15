d2483f848d4d7f24cf34faa505c8c73765c30f1c
iAmJustCam <138799451+iAmJustCam@users.noreply.github.com>
2023-08-15 09:21:29 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,3 @@
-#constants.py module
 # coding: utf-8
 import os
 from typing import NamedTuple, Union
@@ -15,25 +14,24 @@ class ColumnNames(Enum):
     PROJECTED_WINNER = "Projected Winner"
 
 
-class Config(NamedTuple):
-    COLUMNS: list
-    DATE_FORMAT: str
-    CACHE_SIZE: int
+class Environment:
+    """Class to manage environment variables."""
+    
+    @staticmethod
+    def fetch(variable_name: str, default: Union[str, int], expected_type: type) -> Union[str, int]:
+        """Fetch an environment variable, ensuring type safety and providing default values."""
+        raw_value = os.getenv(variable_name, default=default)
+
+        try:
+            return expected_type(raw_value)
+        except ValueError:
+            print(f"Warning: Invalid value for environment variable '{variable_name}'. Using default: {default}.")
+            return default
 
 
-def fetch_env_variable(variable_name: str, default: Union[str, int], expected_type: type) -> Union[str, int]:
-    """Fetch an environment variable, ensuring type safety and providing default values."""
-    raw_value = os.getenv(variable_name, default=default)
-
-    try:
-        return expected_type(raw_value)
-    except ValueError:
-        print(f"Warning: Invalid value for environment variable '{variable_name}'. Using default: {default}.")
-        return default
-
-
-CONSTANTS = Config(
-    COLUMNS=[column.value for column in ColumnNames],
-    DATE_FORMAT=fetch_env_variable("DATE_FORMAT", "%Y-%m-%d", str),
-    CACHE_SIZE=fetch_env_variable("CACHE_SIZE", 100, int)
-)
+class Config:
+    """Class to organize configuration constants."""
+    
+    COLUMNS = [column.value for column in ColumnNames]
+    DATE_FORMAT = Environment.fetch("DATE_FORMAT", "%Y-%m-%d", str)
+    CACHE_SIZE = Environment.fetch("CACHE_SIZE", 100, int)

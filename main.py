2a51125de66bf88265aded00f4b1561096b49373
iAmJustCam <138799451+iAmJustCam@users.noreply.github.com>
2023-08-17 06:56:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import asyncio
+from datetime import datetime, timedelta
+
 import config
+from config import MainConfig, ConfigurationManager
 import Prompter
 import fetcher
 import extractor
@@ -9,53 +13,53 @@ import Evaluator
 import writer
 import logger
 
-def main():
-    # Setup logging
-    logger.setup_logging()
-    log = logger.get_logger(__name__)
+# Setup
+logger.setup_logging()
+log = logger.get_logger(__name__)
+
+async def main():
+    # Load configuration
+    config_manager = ConfigurationManager(MainConfig.CONFIG_FILE_PATH)
 
     # Setup cache
-    backend = cacher.CacheBackend(capacity=100)
-    cache = cacher.Cache(backend=backend, ttl=300)
+    backend = cacher.AsyncCacheBackend(capacity=MainConfig.CACHE_CAPACITY, ttl=MainConfig.CACHE_TTL)
+    cache = cacher.Cache(backend=backend)
+
+    # Setup fetcher
+    scraper_instance = fetcher.MlbScheduleScraper(MainConfig.SCRAPER_URL)
+    fetcher_instance = fetcher.Fetcher(MainConfig.FETCHER, MainConfig.FETCHER.url)
 
     # Setup prompter
-    scraper_instance = fetcher.MlbScheduleScraper()
-    prompter = Prompter.Prompter(scraper_instance)
-    
+    backtest_manager = Prompter.BacktestManager(scraper_instance)
+    prompter = Prompter.Prompter(backtest_manager)
+
     # Setup machine learning instance
-    ml_instance = Trainer.MachineLearning()
-    
-    # Setup fetcher
-    fetcher_instance = fetcher.Fetcher(fetcher.FetcherConfig(), "https://example.com/data")
-    
-    # Setup configuration manager
-    config_manager = config.ConfigurationManager("config.ini")
-    
+    ml_instance = Trainer.MachineLearning(MainConfig.TRAINING_DATA_PATH)
+
     # Setup extractor
-    fetcher_config = fetcher.FetcherConfig(...)
-    async_fetcher = fetcher.AsyncFetcher(fetcher_config)
-    cache_instance = aiocache.SimpleMemoryCache(max_size=100, ttl=3600)
+    async_fetcher = fetcher.AsyncFetcher(MainConfig.FETCHER)
+    cache_instance = cacher.AsyncCacheBackend(capacity=MainConfig.CACHE_CAPACITY)
     team_ranking_extractor = extractor.TeamRankingExtractor(async_fetcher, cache_instance)
 
-    try:
-        # 1. Load configurations
-        configurations = config.load_config()
+    # Setup evaluator
+    evaluator_instance = Evaluator.Backtester(prompter.backtest_period, scraper_instance)
 
+    try:
         # 2. Prompt the user for input and configurations
-        backtest_period = prompter.run() or 7
+        await prompter.run()
 
         # 3. Fetch and extract data
         end_date = datetime.today() - timedelta(days=1)
-        start_date = end_date - timedelta(days=backtest_period - 1)
+        start_date = end_date - timedelta(days=prompter.backtest_period - 1)
         date_values = [
-            (start_date + timedelta(days=i)).strftime(config.Config().DATE_FORMAT)
+            (start_date + timedelta(days=i)).strftime(Config().DATE_FORMAT)
             for i in range((end_date - start_date).days + 1)
         ]
 
         for date in date_values:
-            data = asyncio.run(fetcher_instance.fetch(f"https://example.com/data/{date}", {}))
+            data = await fetcher_instance.fetch(f"{MainConfig.FETCHER.url}/{date}", {})
             extracted_data = extractor.extract_data(data)
-            asyncio.run(cache.put(f"extracted_data_{date}", extracted_data))
+            await cache.put(f"extracted_data_{date}", extracted_data)
 
         # 4. Analyze the data
         validated_matches = analyzer.TeamMatchValidator.validate(extracted_data)
@@ -67,19 +71,20 @@ def main():
         ml_instance.load_training_data(validated_matches)
         ml_instance.evaluate_models()
 
-        # 6. Evaluate the results
-        evaluation_results = Evaluator.evaluate_model(ml_instance.model)
+        # 6. Evaluate the results using the evaluator instance
+        evaluation_results = await evaluator_instance.execute_backtest()
 
         # 7. Write the results or any output
         formatter = writer.JSONFormatter()
         file_writer = writer.FileWriter()
         data_writer = writer.DataWriter(formatter, file_writer)
-        with open("output.json", "w") as file:
-            asyncio.run(data_writer.write_data(file, evaluation_results))
+        with open(MainConfig.OUTPUT_FILE, "w") as file:
+            await data_writer.write_data(file, evaluation_results)
 
     except Exception as e:
         # 8. Log any important information or errors
         log.error(f"An error occurred: {e}")
 
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
